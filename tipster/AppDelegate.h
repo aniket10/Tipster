aee2e2ce976b9d91fbf678c9541e219f733d8671
Aniket Ajagaonkar <aniketa@usesoldmove-lm.corp.yahoo.com>
2014-07-25 21:12:53 -0700

    Initial Commit

---
+++ tipster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipster
+//
+//  Created by Aniket Ajagaonkar on 7/25/14.
+//  Copyright (c) 2014 Aniket Ajagaonkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipster/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipster
+//
+//  Created by Aniket Ajagaonkar on 7/25/14.
+//  Copyright (c) 2014 Aniket Ajagaonkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
